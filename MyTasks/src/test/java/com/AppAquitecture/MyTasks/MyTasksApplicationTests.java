aeb0043e6cdb5469a0b482063a79b8b4dbf7591c
sherngas <sherngas@everis.com>
2020-12-14 15:54:34 +0100

    Proyecto inicializado con Spring Boot

---
+++ MyTasks/src/test/java/com/AppAquitecture/MyTasks/MyTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AppAquitecture.MyTasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
